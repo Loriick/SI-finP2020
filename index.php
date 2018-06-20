a72fb50523d3de157a5dbf410b7d0da9eaed2ec2
loriick <loricktravailleur@gmail.com>
2018-06-20 14:10:40 +0200

    reinitialized files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Space Dust</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
+</head>
+
+<body>
+    <h1>Space Dust</h1>
+    <script src="./assets/js/index.js"></script>
+</body>
+
+</html>
